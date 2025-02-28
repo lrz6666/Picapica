b80bd8911e08f8dcc9938741eb93acca45c7edc5
Agnes <agnesye153@gmail.com>
2025-02-28 00:36:56 -0500

    ad

--- frontend/src/components/Welcome.js
+++ frontend/src/components/Welcome.js
@@ -1,9 +1,17 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
+
 
 /* S */
 const Welcome = () => {
-  const navigate = useNavigate();
+  useEffect(() => {
+    try {
+      (window.adsbygoogle = window.adsbygoogle || []).push({});
+    } catch (e) {
+      console.error("AdSense error:", e);
+    }
+  }, []);
+
 
   return (
     <div className="welcome-container">
@@ -16,6 +24,18 @@ const Welcome = () => {
         After the session, <span style={{ color: "pink" }}></span> download your digital copy and share the fun!
       </p>
       <button onClick={() => navigate("/photobooth")}>START</button>
+
+      {/* Google AdSense Ad Unit */}
+      <ins className="adsbygoogle"
+            style={{ display: "block" }}
+            data-ad-client="ca-pub-7810675993668366"
+            data-ad-slot="7012809521"
+            data-ad-format="auto"
+            data-full-width-responsive="true"></ins>
+
+        <script>
+          {`(adsbygoogle = window.adsbygoogle || []).push({});`}
+        </script>
     </div>
   );
 };
